79ef8e2ef3397fa35cf7288eabb182c269ee5bfa
unknown <nowapio@D02DI1321536EAC.net1.cec.eu.int>
2015-11-12 13:19:45 +0100

    renamed src to app

--- test/main.js
+++ test/main.js
@@ -1,6 +1,6 @@
 requirejs.config({
   paths: {
-    app: '../src',
+    app: '../app',
     jquery: '../../lib/jquery/dist/jquery.min',
     underscore: '../../lib/underscore/underscore-min',
     backbone: '../../lib/backbone/backbone-min',
