9af8170e7c687d068a331352abe17dba7b03a9cb
Piotr Nowak <p.nowak2@gmail.com>
2016-01-08 15:18:25 +0100

    map tab visible by default

--- app/efc/components/app/data/tabs.js
+++ app/efc/components/app/data/tabs.js
@@ -2,11 +2,11 @@ define(function(require) {
   return [{
     title: 'Map',
     identifier: 'map',
-    targetSelector: '.efc-searchable-results-map'
+    targetSelector: '.efc-searchable-results-map',
+    selected: true
   }, {
     title: 'List',
     identifier: 'list',
-    targetSelector: '.efc-searchable-results-list',
-    selected: true
+    targetSelector: '.efc-searchable-results-list'
   }]
 });
