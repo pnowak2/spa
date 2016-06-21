f0341fda30dabf8485d36c0a92807ce9b0861963
Piotr Nowak <p.nowak2@gmail.com>
2016-06-21 17:26:23 +0200

    started eplus/ce response mapper

--- app/eplus-ce/components/landing-page/results/map/searchable-results-map/services/search/searchResult.mapper.js
+++ app/eplus-ce/components/landing-page/results/map/searchable-results-map/services/search/searchResult.mapper.js
@@ -2,7 +2,31 @@ define(function(require) {
   var _ = require('underscore'),
 
     map = function(response) {
-      return response;
+      return {
+        total: response.total,
+        items: _.map(response.items, function(item) {
+          switch (item.type) {
+            case 'cluster':
+              return {
+                type: item.type,
+                itemsCount: item.itemsCount,
+                lat: item.lat,
+                lng: item.lon
+              }
+              break;
+            case 'marker':
+              return {
+                type: item.type,
+                lat: item.lat,
+                lng: item.lon,
+                id: item.id,
+                badges: '',
+                title: item.title
+              }
+              break;
+          }
+        })
+      }
     };
 
   return {
--- app/eplus-ce/components/landing-page/results/map/searchable-results-map/services/search/searchResult.mapper.spec.js
+++ app/eplus-ce/components/landing-page/results/map/searchable-results-map/services/search/searchResult.mapper.spec.js
@@ -6,53 +6,87 @@ define(function(require) {
         total: 0,
         items: []
       },
-      cluster: {
+      clustersOnly: {
         total: 10,
-        items: [
-          // country 1
-          [{
-            type: 'cluster',
-            itemsCount: 6,
-            lat: 51,
-            lng: 24
-          }],
-          // country 2
-          [{
-            type: 'cluster',
-            itemsCount: 3,
-            lat: 53,
-            lng: 26
-          }, {
-            type: 'marker',
-            lat: 54,
-            lng: 27,
-            id: '1',
-          }]
-        ]
+        items: [{
+          type: 'cluster',
+          itemsCount: 10,
+          lat: 51,
+          lon: 24
+        }]
       },
-      noCluster: {
-        total: 3,
-        items: [
-          // country 1
-          [{
-            type: 'marker',
-            lat: 52,
-            lng: 25,
-            id: '1',
-          }],
-          // country 2
-          [{
-            type: 'marker',
-            lat: 53,
-            lng: 26,
-            id: '2',
-          }, {
-            type: 'marker',
-            lat: 54,
-            lng: 27,
-            id: '3',
-          }]
-        ]
+      mixed: {
+        total: 10,
+        items: [{
+          type: 'cluster',
+          itemsCount: 9,
+          lat: 51,
+          lon: 24
+        }, {
+          type: 'marker',
+          lat: 54,
+          lon: 27,
+          id: '1',
+          goodPractice: true,
+          successStory: true,
+          title: 'Project Title',
+          programme: 'Project Programme',
+          actionType: 'Project Action Type',
+          coordinator: 'Project Coordinator',
+          countries: 'PL|DE|BE'
+        }]
+      },
+      markersOnly: {
+        total: 4,
+        items: [{
+          type: 'marker',
+          lat: 55,
+          lon: 28,
+          id: '1',
+          goodPractice: false,
+          successStory: false,
+          title: 'Project Title 1',
+          programme: 'Project Programme 1',
+          actionType: 'Project Action Type 1',
+          coordinator: 'Project Coordinator 1',
+          countries: 'PL|DE|BE'
+        }, {
+          type: 'marker',
+          lat: 56,
+          lon: 29,
+          id: '2',
+          goodPractice: true,
+          successStory: false,
+          title: 'Project Title 2',
+          programme: 'Project Programme 2',
+          actionType: 'Project Action Type 2',
+          coordinator: 'Project Coordinator 2',
+          countries: 'PL|DE|BE'
+        }, {
+          type: 'marker',
+          lat: 57,
+          lon: 30,
+          id: '3',
+          goodPractice: false,
+          successStory: true,
+          title: 'Project Title 3',
+          programme: 'Project Programme 3',
+          actionType: 'Project Action Type 3',
+          coordinator: 'Project Coordinator 3',
+          countries: 'PL|DE|BE'
+        }, {
+          type: 'marker',
+          lat: 58,
+          lon: 31,
+          id: '4',
+          goodPractice: true,
+          successStory: true,
+          title: 'Project Title 4',
+          programme: 'Project Programme 4',
+          actionType: 'Project Action Type 4',
+          coordinator: 'Project Coordinator 4',
+          countries: 'PL|DE|BE'
+        }]
       }
     };
 
@@ -65,12 +99,62 @@ define(function(require) {
 
     describe('api', function() {
       describe('.map', function() {
-        beforeEach(function() {
-
-        });
-
         it('should be defined', function() {
           expect(searchResultMapper.map).toEqual(jasmine.any(Function));
+        });
+
+        describe('clustered only response', function() {
+          beforeEach(function() {
+            this.mapped = searchResultMapper.map(testResponses.clustersOnly);
+          });
+
+          it('should have total property', function() {
+            expect(this.mapped).toEqual(jasmine.objectContaining({
+              total: 10
+            }));
+          });
+
+          it('should have items property', function() {
+            expect(this.mapped.items).toEqual(jasmine.any(Array));
+            expect(this.mapped.items.length).toEqual(1);
+          });
+
+          it('should have cluster item with proper data', function() {
+            expect(this.mapped.items[0]).toEqual({
+              type: 'cluster',
+              itemsCount: 10,
+              lat: 51,
+              lng: 24
+            });
+          });
+        });
+
+        describe('markers only response', function() {
+          beforeEach(function() {
+            this.mapped = searchResultMapper.map(testResponses.markersOnly);
+          });
+
+          it('should have total property', function() {
+            expect(this.mapped).toEqual(jasmine.objectContaining({
+              total: 4
+            }));
+          });
+
+          it('should have items property', function() {
+            expect(this.mapped.items).toEqual(jasmine.any(Array));
+            expect(this.mapped.items.length).toEqual(4);
+          });
+
+          it('should have marker item with proper data', function() {
+            expect(this.mapped.items[0]).toEqual({
+              type: 'marker',
+              lat: 55,
+              lng: 28,
+              id: '1',
+              badges: '',
+              title: 'Project Title 1'
+            });
+          });
         });
       });
     });
