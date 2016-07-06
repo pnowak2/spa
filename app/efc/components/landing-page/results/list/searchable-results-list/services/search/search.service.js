fd5cb2d0203f069cc4e3646078d443ad65f2447d
Piotr Nowak <p.nowak2@gmail.com>
2016-07-06 10:47:47 +0200

    removed garbage

--- app/efc/components/landing-page/results/list/searchable-results-list/services/search/search.service.js
+++ app/efc/components/landing-page/results/list/searchable-results-list/services/search/search.service.js
@@ -10,29 +10,6 @@ define(function(require) {
       criteria = criteria || {};
 
       return new RSVP.Promise(function(resolve, reject) {
-
-        // resolve(searchResultMapper.map({
-        //   iTotalRecords: '2',
-        //   aaData: [
-        //     [
-        //       '11',
-        //       'Title 1',
-        //       'Description 1',
-        //       '2011',
-        //       'PL|DE',
-        //       'Organisation'
-        //     ],
-        //     [
-        //       '16',
-        //       'Title 2',
-        //       'Description 2',
-        //       '2012',
-        //       'FR',
-        //       'Organisation'
-        //     ],
-        //   ]
-        // }));
-
         $.ajax({
           url: constants.urls.SEARCH_LIST,
           dataType: 'json',
