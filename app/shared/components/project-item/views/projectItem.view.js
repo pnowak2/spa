5f7b538ca12f648834f5b39655ea94226391cc6d
Piotr Nowak <p.nowak2@gmail.com>
2016-08-24 16:23:08 +0200

    putting countries in project item component

--- app/shared/components/project-item/views/projectItem.view.js
+++ app/shared/components/project-item/views/projectItem.view.js
@@ -1,20 +1,31 @@
 define(function(require) {
   var Backbone = require('backbone'),
     Mustache = require('mustache'),
+    FlagsComponent = require('app/shared/components/flags/main.component'),
     tpl = require('text!../templates/projectItem.tpl.html');
 
   return Backbone.View.extend({
     className: 'vlr-project-item',
 
-    initialize: function (options) {
-    	options = options || {};
+    initialize: function(options) {
+      options = options || {};
 
-    	this.options = _.extend({}, options);
+      this.options = _.extend({}, options);
     },
 
     render: function() {
-      var html = Mustache.render(tpl, this.options.data);
+      var html = Mustache.render(tpl, this.options.data),
+        flagsComponent = new FlagsComponent([{
+          code: 'pl',
+          fullName: 'Poland'
+        }, {
+          code: 'de',
+          fullName: 'Germany'
+        }]);
+
       this.$el.html(html);
+
+      this.$el.find('.vlr-project-item__countries-container').html(flagsComponent.render().view.el);
 
       return this;
     }
