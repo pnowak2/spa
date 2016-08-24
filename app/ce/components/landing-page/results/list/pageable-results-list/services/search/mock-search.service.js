6f753a322b0bae29d7954e5032ee039b372adf97
Piotr Nowak <p.nowak2@gmail.com>
2016-08-24 09:25:59 +0200

    added chance random generator

--- app/ce/components/landing-page/results/list/pageable-results-list/services/search/mock-search.service.js
+++ app/ce/components/landing-page/results/list/pageable-results-list/services/search/mock-search.service.js
@@ -1,17 +1,20 @@
 define(function(require) {
   var RSVP = require('rsvp'),
-    searchResultMapper = require('./searchResult.mapper');
+    searchResultMapper = require('./searchResult.mapper'),
+    chance = require('chance');
 
   return {
     search: function(criteria) {
       return new RSVP.Promise(function(resolve, reject) {
+        var chance = new Chance();
+
         resolve(searchResultMapper.map({
           "iTotalRecords": "1242",
           "iTotalDisplayRecords": "1242",
           "took": "60 ms",
           "aaData": [
-            ["workspace:\/\/SpacesStore\/40ef3df0-70fc-40e6-aea9-79769cc4c84d", "Arena World " + criteria.currentPage, "In a rift in time, between universes, exists a place where Gladiators from the known dimensions meet, to demonstrate their absolute skill in combat and survival against deadly foes enslaved from a...", "2013", "DK", "true"],
-            ["workspace:\/\/SpacesStore\/9364c9e5-9ac9-4d26-95c5-d412ffbb222c", "Berlinale Co-Production Market " + criteria.currentPage, "The Berlinale Co-Production Market is the Berlin International Film Festival\u0092s service and networking platform for producers, financiers, broadcasters, distributors and sales agents working in int...", "2014", "DE|ES", "false"],
+            [chance.timestamp() + '', chance.sentence({words: 15}), chance.paragraph(), chance.natural({min: 2002, max: 2016}) + '', chance.country(), chance.bool() + ''],
+            ["workspace:\/\/SpacesStore\/9364c9e5-9ac9-4d26-95c5-d412ffbb222c", chance.paragraph(), "The Berlinale Co-Production Market is the Berlin International Film Festival\u0092s service and networking platform for producers, financiers, broadcasters, distributors and sales agents working in int...", "2014", "DE|ES", "false"],
             ["workspace:\/\/SpacesStore\/70d7d5aa-7439-402c-b02a-50acf1b68821", "Translation of Eight Books into Macedonian Language " + criteria.currentPage, "The project encompasses translation into Macedonian language of titles by eight significant contemporary European authors, and promotion of the publications. Selected works come from various geogr...", "2014", "MK", "false"],
             ["workspace:\/\/SpacesStore\/f559cfd1-14f9-4be5-be52-67a44457d5f0", "CLOSE RELATIONS " + criteria.currentPage, "Documentary director Vitaly Mansky returns to his native Ukraine to visit his mother, aunts, uncles and cousins in Lviv, Odessa,  Crimea and Donetsk to find out what lies beneath the current confl...", "2014", "LV|EE", "false"],
             ["workspace:\/\/SpacesStore\/edce7d21-961e-448b-8b24-43a71b00fb54", "Revolting Rhymes " + criteria.currentPage, "\u0091Revolting Rhymes\u0092 are 2 x 26\u0092 CGI animated half hour specials for television based on the much loved children's book written by celebrated UK author Roald Dahl, and illustrated by Quentin Blake. ...", "2014", "UK", "false"],
