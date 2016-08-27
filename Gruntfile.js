5fbbb619591e4257c15c529d062932c66bac9654
Piotr Nowak <p.nowak2@gmail.com>
2016-08-27 16:17:48 +0200

    added grunt requirejs integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,52 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: '.',
+          paths: {
+            app: './app',
+            jquery: 'lib/jquery/jquery.min',
+            blockUI: 'lib/jquery-blockUI/jquery.blockUI.min',
+            underscore: 'lib/underscore/underscore-min',
+            backbone: 'lib/backbone/backbone-min',
+            mustache: 'lib/mustache.js/mustache.min',
+            text: 'lib/text/text',
+            select2: 'lib/select2/js/select2.full.min',
+            rsvp: 'lib/rsvp/rsvp.min',
+            modernizr: 'lib/modernizr/modernizr-custom.min',
+            leaflet: 'lib/leaflet/leaflet',
+            leafletprunecluster: 'lib/leaflet.prunecluster/dist/PruneCluster',
+            leafletfullscreen: 'lib/leaflet.fullscreen/dist/Leaflet.fullscreen',
+            leafleteasybutton: 'lib/leaflet.easybutton/dist/easy-button',
+            chance: 'lib/chance/chance.min',
+
+            requireLib: 'lib/requirejs/require.min'
+          },
+          shim: {
+            modernizr: {},
+            leafletprunecluster: {
+              deps: ['leaflet']
+            },
+            leafletfullscreen: {
+              deps: ['leaflet']
+            },
+            leafleteasybutton: {
+              deps: ['leaflet']
+            }
+          },
+          generateSourceMaps: true,
+          preserveLicenseComments: false,
+          include: "requireLib",
+          name: "ce.landing-page.main",
+          out: "dist/vlr-app.min.js"
+        }
+      }
+    }
   });
 
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  grunt.registerTask('default', ['requirejs']);
 };
