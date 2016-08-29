2e3da6065624104c45b9bf2de50a437849e9ce85
Piotr Nowak <p.nowak2@gmail.com>
2016-08-29 11:26:29 +0200

    added jshint browser mode

--- app/ce/components/landing-page/results/list/results-list/views/resultsList.view.js
+++ app/ce/components/landing-page/results/list/results-list/views/resultsList.view.js
@@ -1,5 +1,6 @@
 define(function(require) {
-  var Backbone = require('backbone'),
+  var _ = require('underscore'),
+    Backbone = require('backbone'),
     Mustache = require('mustache'),
     tpl = require('text!../templates/results-list.tpl.html');
 
