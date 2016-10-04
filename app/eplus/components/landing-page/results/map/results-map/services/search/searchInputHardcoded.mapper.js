1e2c7feeb292da5d74fd835b5ad0668758fb9e8c
Piotr Nowak <p.nowak2@gmail.com>
2016-10-04 14:03:24 +0200

    Fixed hardcoded params to eplus map.

--- app/eplus/components/landing-page/results/map/results-map/services/search/searchInputHardcoded.mapper.js
+++ app/eplus/components/landing-page/results/map/results-map/services/search/searchInputHardcoded.mapper.js
@@ -7,11 +7,16 @@ var _ = require('underscore'),
     searchInputMapper = require('./searchInput.mapper'),
 
     map = function(input) {
-      var mapped = searchInputMapper.map(input);
+      var mapped = searchInputMapper.map(input),
+        level1 = mapped['FILTER-LEVEL1'];
+
+      if(_.isEmpty(level1) || level1.indexOf('31046216') !== -1) {
+        level1 = '31046216';
+      }
 
       return _.extend({}, mapped, {
         'searchType': 'advanced',
-        'FILTER-LEVEL1': '31046216'
+        'FILTER-LEVEL1': level1
       });
     };
 
--- app/eplus/components/landing-page/results/map/results-map/services/search/searchInputHardcoded.mapper.spec.js
+++ app/eplus/components/landing-page/results/map/results-map/services/search/searchInputHardcoded.mapper.spec.js
@@ -12,33 +12,57 @@ define(function(require) {
 
     describe('api', function() {
       describe('.map()', function() {
-        beforeEach(function() {
-          spyOn(searchInputMapper, 'map').and.returnValue({
-            'KEYWORD': 'foo',
-            'searchType': 'bar',
-            'FILTER-LEVEL1': 'dar'
-          });
-        });
-
         it('should be defined', function() {
           expect(searchInputHardcodedMapper.map).toEqual(jasmine.any(Function));
         });
 
         it('should pass other properties without changes', function() {
+          spyOn(searchInputMapper, 'map').and.returnValue({
+            'KEYWORD': 'foo'
+          });
+
           expect(searchInputHardcodedMapper.map()).toEqual(jasmine.objectContaining({
             'KEYWORD': 'foo'
           }));
         });
 
         it('should override search type', function() {
+          spyOn(searchInputMapper, 'map').and.returnValue({
+            'searchType': 'bar'
+          });
+
           expect(searchInputHardcodedMapper.map()).toEqual(jasmine.objectContaining({
             'searchType': 'advanced'
           }));
         });
 
-        it('should override level 1', function() {
+        it('should hardcode level 1 if level1 is empty', function() {
+          spyOn(searchInputMapper, 'map').and.returnValue({
+            'FILTER-LEVEL1': ''
+          });
+
           expect(searchInputHardcodedMapper.map()).toEqual(jasmine.objectContaining({
             'FILTER-LEVEL1': '31046216'
+          }));
+        });
+
+        it('should hardcode level 1 if level1 is set to eplus programme', function() {
+          spyOn(searchInputMapper, 'map').and.returnValue({
+            'FILTER-LEVEL1': '31046216;12345678'
+          });
+
+          expect(searchInputHardcodedMapper.map()).toEqual(jasmine.objectContaining({
+            'FILTER-LEVEL1': '31046216'
+          }));
+        });
+
+        it('should not hardcode level 1 if level1 is does not contain eplus programme', function() {
+          spyOn(searchInputMapper, 'map').and.returnValue({
+            'FILTER-LEVEL1': '12345678'
+          });
+
+          expect(searchInputHardcodedMapper.map()).toEqual(jasmine.objectContaining({
+            'FILTER-LEVEL1': '12345678'
           }));
         });
       });
